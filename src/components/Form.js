260e6b73650a6cfb988c2c287d46610c3511bd05
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-03-07 17:17:36 -0300

    Requisito 2, lint com erro proposital devido a prop ainda não utilizada

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,121 @@
 import React from 'react';
-import Input from './Input';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      onInputChange, onSaveButtonClick } = this.props;
     return (
-      <Input />
+      <>
+        <label>
+          Nome:
+          <input
+            data-testid="name-input"
+            type="text"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
+        </label>
+        <br />
+        <label>
+          Descrição:
+          <input
+            data-testid="description-input"
+            type="textarea"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
+        </label>
+        <br />
+        <label>
+          Atributo 1:
+          <input
+            data-testid="attr1-input"
+            type="number"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <br />
+        <label>
+          Atributo 2:
+          <input
+            data-testid="attr2-input"
+            type="number"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <br />
+        <label>
+          Atributo 3:
+          <input
+            data-testid="attr3-input"
+            type="number"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <br />
+        <label>
+          Imagem:
+          <input
+            data-testid="image-input"
+            type="text"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
+        </label>
+        <br />
+        <label>
+          Raridade:
+          <select
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Super Trunfo:
+          <input
+            data-testid="trunfo-input"
+            type="checkbox"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
+        </label>
+        <br />
+        <button
+          disabled={ isSaveButtonDisabled }
+          data-testid="save-button"
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
+      </>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,57 +1,68 @@
-import React from 'react';
+// import React from 'react';
+// import Form from './Form';
 
-class Input extends React.Component {
-  render() {
-    return (
-      <>
-        <label>
-          Nome:
-          <input data-testid="name-input" type="text" id="" name="" />
-        </label>
-        <br />
-        <label>
-          Descrição:
-          <input data-testid="description-input" type="textarea" id="" name="" />
-        </label>
-        <br />
-        <label>
-          Atributo 1:
-          <input data-testid="attr1-input" type="number" id="" name="" />
-        </label>
-        <br />
-        <label>
-          Atributo 2:
-          <input data-testid="attr2-input" type="number" id="" name="" />
-        </label>
-        <br />
-        <label>
-          Atributo 3:
-          <input data-testid="attr3-input" type="number" id="" name="" />
-        </label>
-        <br />
-        <label>
-          Imagem:
-          <input data-testid="image-input" type="text" id="" name="" />
-        </label>
-        <br />
-        <label>
-          Raridade:
-          <select data-testid="rare-input">
-            <option value="normal">normal</option>
-            <option value="raro">raro</option>
-            <option value="muito raro">muito raro</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Super Trunfo:
-          <input data-testid="trunfo-input" type="checkbox" id="" name="" />
-        </label>
-        <br />
-        <button data-testid="save-button">Salvar</button>
-      </>
-    );
-  }
-}
+// class Input extends React.Component {
+//   constructor() {
+//     super();
+//     console.log('teste');
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-export default Input;
+//   handleClick() {
+//     console.log('clicou');
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <label>
+//           Nome:
+//           <input data-testid="name-input" type="text" value={ cardName } onChange={ onInputChange } >
+//         </label>
+//         <br />
+//         <label>
+//           Descrição:
+//           <input data-testid="description-input" type="textarea" id="" name="" />
+//         </label>
+//         <br />
+//         <label>
+//           Atributo 1:
+//           <input data-testid="attr1-input" type="number" id="" name="" />
+//         </label>
+//         <br />
+//         <label>
+//           Atributo 2:
+//           <input data-testid="attr2-input" type="number" id="" name="" />
+//         </label>
+//         <br />
+//         <label>
+//           Atributo 3:
+//           <input data-testid="attr3-input" type="number" id="" name="" />
+//         </label>
+//         <br />
+//         <label>
+//           Imagem:
+//           <input data-testid="image-input" type="text" id="" name="" />
+//         </label>
+//         <br />
+//         <label>
+//           Raridade:
+//           <select data-testid="rare-input">
+//             <option value="normal">normal</option>
+//             <option value="raro">raro</option>
+//             <option value="muito raro">muito raro</option>
+//           </select>
+//         </label>
+//         <br />
+//         <label>
+//           Super Trunfo:
+//           <input data-testid="trunfo-input" type="checkbox" id="" name="" />
+//         </label>
+//         <br />
+//         <button onClick={this.handleClick} data-testid="save-button">Salvar</button>
+//       </>
+//     );
+//   }
+// }
+
+// export default Input;
