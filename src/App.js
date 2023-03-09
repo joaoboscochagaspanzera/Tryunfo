ca1e385e54a9da40a65da7614209e0bd420e0988
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-03-09 07:54:03 -0300

    Requisito 3 refeito, lint corrigido

--- src/App.js
+++ src/App.js
@@ -3,13 +3,52 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: true,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
   render() {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled } = this.state;
     return (
       <>
         <div>
           <h1>Tryunfo</h1>
         </div>
-        <Form />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+        />
         <Card />
       </>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,6 +6,7 @@ class Form extends React.Component {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
       cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
       onInputChange, onSaveButtonClick } = this.props;
+    console.log(hasTrunfo);
     return (
       <>
         <label>
@@ -14,7 +15,7 @@ class Form extends React.Component {
             data-testid="name-input"
             type="text"
             value={ cardName }
-            onChange={ onInputChange }
+            onClick={ onInputChange }
           />
         </label>
         <br />
