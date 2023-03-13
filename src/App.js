5f50d89907858b6ca4d4830acbf9d2f1c546a357
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-03-13 09:29:41 -0300

    Requisito 6, lint verificado

--- src/App.js
+++ src/App.js
@@ -22,11 +22,25 @@ class App extends React.Component {
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
   }
 
   onInputChange = ({ target }) => {
     this.setState({
       [target.name]: target.value,
+    });
+  };
+
+  onSaveButtonClick = () => {
+    this.setState({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
     });
   };
 
@@ -61,6 +75,7 @@ class App extends React.Component {
           hasTrunfo={ hasTrunfo }
           isSaveButtonDisabled={ !isFormValid }
           onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
         />
         <Card
           cardName={ cardName }
